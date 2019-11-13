ee8b657b9ba27591572b6dfadd9d11dae6034ce6
Daniel Rowe <danielrowetech@gmail.com>
2019-11-12 19:29:21 -0800

    init commit. 9 working streams.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactPlayer from "react-player";
+import streams from "./streams.js";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="title has-text-white">Seattle Traffic Cams</h1>
+      <div className="players">
+        {streams.map(stream => (
+          <ReactPlayer
+            url={stream.url}
+            controls={true}
+            playing={true}
+            className="stream-player"
+            width="18em"
+            height="10.125em"
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/streams.js
@@ -0,0 +1,49 @@
+const streams = [
+  {
+    name: "23rd Ave & E Adler",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/23_Alder_NS.stream/playlist.m3u8"
+  },
+  {
+    name: "Leary Way NW & NW 43rd St",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/Leary_NW_43_EW.stream/playlist.m3u8"
+  },
+  {
+    name: "Broadway & E Roy St",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/Broadway_E_Roy_NS.stream/playlist.m3u8"
+  },
+  {
+    name: "4th Ave N & Dexter Ave N",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/Westlake_N_Dexter_NS.stream/playlist.m3u8"
+  },
+  {
+    name: "Evanston Ave N & N 36th St",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/Evanston_N_36_EW.stream/playlist.m3u8"
+  },
+  {
+    name: "4th Ave & Cherry St NS",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/4_Cherry_NS.stream/playlist.m3u8"
+  },
+  {
+    name: "15th Ave NW & NW Leary Way",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/15_NW_Leary_EW.stream/playlist.m3u8"
+  },
+  {
+    name: "15th Ave NW & NW Market St",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/15_NW_Market_EW.stream/playlist.m3u8"
+  },
+  {
+    name: "15th Ave NW & NW 65th St NS",
+    url:
+      "https://58cc2dce193dd.streamlock.net/live/15_NW_65_NS.stream/playlist.m3u8"
+  }
+];
+
+export default streams;
